fafadea23621f0f46c24c7bf5beed28fa3fe26b6
garymartinezgonzalez <gary.martinezgonzalez@gmail.com>
2019-06-01 14:48:12 -0500

    added one route on map

---
+++ client/src/components/routes.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BoxDropDown from './components/Dropdown.jsx';
+import { Button } from 'reactstrap';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import combiData from './Data.js';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
